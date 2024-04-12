349a690e2b468ad6c27979fbb0d165e5586ce6ec
JPMartinez <jpmartinez@uchicago.edu>
2024-04-12 15:39:29 -0500

    GitHub commit testing - extract nyt data

---
+++ backend/ingestion/extract_nyt_data.py
@@ -0,0 +1,19 @@
+import os 
+from typing import List
+import requests
+from lxml.html import html_parser
+import json
+
+#def create_nyt_client()
+
+def extract_table_mta_json(end_point:str,api_key = None)-> str:
+    """
+    Extracts the json response from a API request to the NYC Data portal 
+    """
+    resp = requests.get(end_point)
+    resp_json = json.loads(resp.text)
+    return resp_json
+
+if __name__ == "__main__":
+    resp = extract_table_mta_json(end_point="https://data.ny.gov/resource/uu7b-3kff.json")
+    print(resp)
