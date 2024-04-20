1a2c161c60f01e10d64202aa845c4217ba7bf58a
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2024-04-20 18:35:50 -0500

    adding tests for portland ridership data ingestion

---
+++ backend/tests/test_portland_ingest.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def dataset():
+
+    file_path = "/Users/jimenasalinas/Library/CloudStorage/Box-Box/Route Rangers/Transit dataset exploration/Portland Ridership Data/portland_ridership/sorted_data.json"
+
+    data = pd.read_json(file_path)
+
+    return data
+
+
+def test_no_duplicates(dataset):
+
+    dataset.reset_index(drop=True, inplace=True)
+
+    duplicate_check = dataset.duplicated(subset=["location_id", "date"])
+
+    duplicate_rows = dataset[duplicate_check]
+
+    assert duplicate_rows.empty, "Duplicates found at ['location_id',  'date'] level."
