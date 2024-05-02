6077998fbc85b500ff18d53792b88e77ebb5a30f
JPMartinez <jpmartinez@uchicago.edu>
2024-05-02 10:04:07 -0500

    Added city choice choices on Route and Station models

--- app/route_rangers_api/models.py
+++ app/route_rangers_api/models.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.db import models
 
+CITIES_CHOICES = {"CHI": "Chicago", "NYC": "New York", "PDX": "Portland"}
+
 #################################
 ###### DEMOGRAPHIC MODELS #######
 #################################
@@ -9,22 +11,36 @@ class Demographics(models.Model):
     """
     Class to represent demographic data pulled from the ACS Survey
     """
-    census_tract = models.CharField(
+
+    census_block = models.CharField(
         max_length=15
-    )  # Check length of census tract if its uniform to enforce it here
+    )  # Check length of census block if its uniform to enforce it here
     state = models.CharField(
         max_length=15
     )  # Check if it's worth to keep or if we should add a method
     county = models.CharField(max_length=15)  # Same as above
     median_income = models.IntegerField()
-    transportation_to_work = models.CharField(
-        verbose_name="Means of Transportation to Work"
+    transportation_to_work = models.IntegerField(
+        verbose_name="Means of Transportation to Work Total"
     )
-    work_commute_time = models.FloatField(verbose_name="Time of commuto to work")
+    transportation_to_work_car = models.IntegerField(
+        verbose_name="Means of Transportation to Work: Car", null=True
+    )
+    transportation_to_work_public = models.IntegerField(
+        verbose_name="Means of Transportation to Work: Public Transportation", null=True
+    )
+    transportation_to_work_bus = models.IntegerField(
+        verbose_name="Means of Transportation to Work: Bus", null=True
+    )
+    transportation_to_work_subway = models.IntegerField(
+        verbose_name="Means of Transportation to Work: subway", null=True
+    )
+    work_commute_time = models.FloatField(verbose_name="Time of commute to work")
     vehicles_available = models.IntegerField()
     disability_status = models.IntegerField(
         verbose_name="Number of people with disability"
     )
+
 
 #################################
 ######## TRANSIT MODELS #########
@@ -35,14 +51,15 @@ class TransitRoute(models.Model):
     """
     Class that represent subway lines and bus routes
     """
-    city = models.CharField(max_length=30)
+
+    city = models.CharField(max_length=30, choices=CITIES_CHOICES)
     route_id = models.CharField(max_length=30)
     geo_representation = models.LineStringField()
     mode = models.CharField(verbose_name="Mode of transportation", max_length=10)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["city","route_id"],name="city route id")
+            models.UniqueConstraint(fields=["city", "route_id"], name="city route id")
         ]
 
 
@@ -51,13 +68,14 @@ class TransitStation(models.Model):
     Class that represents bus stops and station complex
     (i.e. CTA - Roosevelt)
     """
-    city = models.CharField(max_length=30)
+
+    city = models.CharField(max_length=30, choices=CITIES_CHOICES)
     station = models.CharField(max_length=30)
-    location = models.PointField()
+    location = models.PointField(null=True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["city","station"],name="city station")
+            models.UniqueConstraint(fields=["city", "station"], name="city station")
         ]
 
 
@@ -68,6 +86,7 @@ class StationRouteRelation(models.Model):
     (i.e. row 1: CTA Roosevelt - Green Line
      row 2: CTA Roosevelt - Red Line)
     """
+
     station = models.ForeignKey(TransitStation, on_delete=models.PROTECT)
     route = models.ForeignKey(TransitRoute, on_delete=models.PROTECT)
 
@@ -76,6 +95,7 @@ class RidershipRoute(models.Model):
     """
     Class that represent ridership at the route level
     """
+
     route = models.ForeignKey(TransitRoute, on_delete=models.PROTECT)
     date = models.DateField()
     ridership = models.IntegerField()
@@ -85,6 +105,7 @@ class RidershipStation(models.Model):
     """
     Class that represent ridership at the station level
     """
+
     station = models.ForeignKey(TransitStation, on_delete=models.PROTECT)
     date = models.DateField()
     ridership = models.IntegerField()
@@ -94,15 +115,16 @@ class BikeStation(models.Model):
     """
     Class that represent bike sharing docking stations
     """
+
     station_id = models.CharField(max_length=30, primary_key=True)
     location = models.PointField()
-    census_tract = models.CharField(max_length=30, null=True)
 
 
 class BikeRidership(models.Model):
     """
     Class that represent bike sharing ridership
     """
+
     station = models.ForeignKey(BikeStation, on_delete=models.PROTECT)
     date = models.DateField()
     n_started = models.IntegerField()
@@ -118,6 +140,7 @@ class Survey(models.Model):
     """
     Class that represents surveys deployed
     """
+
     name = models.CharField(max_length=30)
     created_at = models.DateTimeField("Created at", auto_now_add=True)
     questionnaire = models.JSONField()
@@ -127,6 +150,7 @@ class SurveyAnswer(models.Model):
     """
     Class that represents answers to surveys
     """
+
     user_id = models.CharField(max_length=30)
     response_date = models.DateTimeField("Survey response date", auto_now_add=True)
     city = models.CharField(max_length=30)
@@ -138,6 +162,7 @@ class PlannedRoute(models.Model):
     """
     Class that represents answers to 'Plan your route' feature
     """
+
     user_id = models.CharField(max_length=30)
     response_date = models.DateTimeField("Survey response date", auto_now_add=True)
     route = models.LineStringField()
