01927adab73b05a3ce875ffc04fb92dabfac37ff
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2024-05-07 13:09:02 -0500

    some stations not in stations db

--- app/scripts/portland_ingest.py
+++ app/scripts/portland_ingest.py
@@ -18,3 +18,64 @@ VARIABLES:
     and day type (Saturday, Sunday, and Weekday). In order to fit the data model
     for the project which is at the datetime level, the data has been mapped to dates in 2023.
 """
+
+import json
+import sys
+import os
+from requests.models import Response
+import django
+import datetime
+import pytz
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geodjango.settings")
+
+django.setup()
+
+
+from route_rangers_api.models import TransitStation, RidershipStation
+
+
+def ingest_pdx_ridership_data(json_file_path: str) -> None:
+    """
+    Ingest portland ridership data from extracted JSON file
+    into Django database
+    """
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+
+    for record in data:
+        # Get the id from TransitStation using station_id + city = 'PDX'
+        foreign_key = TransitStation.objects.get(
+            station_id=record["station_id"], city="PDX"
+        ).id
+
+        print(f"ingesting ridership data for station {record['station_id']}...")
+        # For each row, create a RidershipStation object
+        timestamp_seconds = int(record["date"]) / 1000
+        date_obj = datetime.datetime.fromtimestamp(timestamp_seconds)
+        # to match model
+
+        ridership_obj = RidershipStation.objects.create(
+            id=foreign_key,
+            date=date_obj,
+            ridership=record["ridership"],
+            station_id=record["station_id"],
+        )
+        print(ridership_obj)
+    print("Ingestion complete")
+
+
+def run():
+    """
+    Ingest PDX ridership data into Django db
+    """
+    json_file_path = "/Users/jimenasalinas/Library/CloudStorage/Box-Box/Route Rangers/Transit dataset exploration/Portland Ridership Data/portland_ridership/Portland_ridership.json"
+    ingest_pdx_ridership_data(json_file_path)
+
+
+if __name__ == "__main__":
+    run()
