9008c32d0eb15e9657ffacf989ef9322224ea935
lisettesolis-capp <111702116+lisettesolis-capp@users.noreply.github.com>
2024-05-10 12:48:22 -0500

    map.js function, small icon

--- app/route_rangers_api/static/map.js
+++ app/route_rangers_api/static/map.js
@@ -1,15 +1,30 @@
-    // Initialize the map with Chicago as the center
-    var map = L.map('map').setView({Coordinates}, 13); // [latitude, longitude], zoom level
-    
-    // Add a tile layer
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
-    }).addTo(map);
+export function initializeMap(coordinates, stations, iconUrl) {
+  // Initialize the map with the given coordinates as the center
+  var map = L.map('map').setView(coordinates, 13);
+
+  // Add a tile layer
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19,
+  }).addTo(map);
+
+  // Custom icon for smaller markers
+  var smallIcon = L.icon({
+    iconUrl: iconUrl, // URL to a smaller icon image
+    iconSize: [12, 12], // Set the icon size you want
+    iconAnchor: [6, 6], // Adjust the anchor point if necessary
+  });
 
     // Loop through the stations and add a smaller marker for each
-    var stations_data = {stations};
-    stations_data.forEach(function(station) {
-        var marker = L.marker([station[0], station[1]]).addTo(map);
-        });
+    stations.forEach(function(station) {
+        var marker = L.marker([station[0], station[1]], {icon: smallIcon}).addTo(map);
+    });
+
+}
+
+//  code to use standard marker
+//   stations.forEach(function (station) {
+//     L.marker([station[0], station[1]]).addTo(map);
+//   });
