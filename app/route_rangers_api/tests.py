47a2f67e6eb49f236bf75fc76d3944c25cc2cfff
lisettesolis-capp <111702116+lisettesolis-capp@users.noreply.github.com>
2024-05-12 14:59:56 -0500

    remove django dir

--- app/route_rangers_api/tests.py
+++ app/route_rangers_api/tests.py
@@ -8,10 +8,203 @@ def test_fake():
 
 @pytest.mark.django_db
 def test_hello_view(client):
-    url = reverse("/map")
+    url = reverse("/")
 
     response = client.get(url)
-    import pdb
-
-    pdb.set_trace()
     assert response.status_code == 200
+
+
+import pytest
+
+from app.scripts.extract_scheduled_gtfs import (
+    # ingest_gtfs_feed,
+    CTA_URL,
+    METRA_URL,
+    get_gtfs_component_dfs,
+    # add_extra_columns,
+    # combine_different_feeds,
+)
+
+
+# use a fixture to pre_load some relevant feed ingestions
+# https://docs.pytest.org/en/6.2.x/fixture.html
+# https://stackoverflow.com/questions/17801300/how-to-run-a-method-before-all-tests-in-all-classes
+@pytest.fixture(scope="session", autouse=True)
+def metra_feed():
+    feed_city, feed_agency, feed = ingest_gtfs_feed(METRA_URL)
+    return feed_city, feed_agency, feed
+
+
+@pytest.fixture(scope="session")
+def cta_feed():
+    """
+    Because CTA shapes.txt file is so large, this will take several seconds,
+    and should not be called until a test needs it.
+    """
+    feed_city, feed_agency, feed = ingest_gtfs_feed(CTA_URL)
+    return feed_city, feed_agency, feed
+
+
+@pytest.mark.skip(reason="refactor currently breaking test")
+def test_feed_ingest(metra_feed):
+    feed_city, feed_agency, feed = metra_feed
+    assert feed_city == "Chicago"
+    assert feed_agency == "Metra"
+    assert feed is not None
+    # TODO: assert that feed object is well-formed
+
+
+@pytest.mark.skip(reason="refactor currently breaking test")
+def test_get_gtfs_component_dfs_metra(metra_feed):
+    # Metra feed has neither transfers.txt nor geometrizable routes
+    feed_city, feed_agency, feed = metra_feed
+    components_dict = get_gtfs_component_dfs(feed_city, feed_agency, feed)
+    components = components_dict.keys()
+    assert "routes" in components
+    assert "trips" in components
+    assert "stops" in components
+    assert "stop_times" in components
+    assert "shapes" in components
+    assert "transfers" not in components
+    assert "shape_geometries" not in components
+
+
+@pytest.mark.skip(reason="refactor currently breaking test")
+def test_get_gtfs_component_dfs_cta(cta_feed):
+    """TODO: refactor this so it is DRY"""
+    feed_city, feed_agency, feed = cta_feed
+    components_dict = get_gtfs_component_dfs(feed_city, feed_agency, feed)
+    components = components_dict.keys()
+    assert "routes" in components
+    assert "trips" in components
+    assert "stops" in components
+    assert "stop_times" in components
+    assert "shapes" in components
+    # CTA has BOTH transfers.txt AND geometrizable routes
+    assert "transfers" in components
+    assert "shape_geometries" in components
+
+
+@pytest.mark.skip(reason="refactor currently breaking test")
+def test_add_extra_columns(metra_feed):
+    feed_city, feed_agency, feed = metra_feed
+    component = add_extra_columns(feed_city, feed_agency, feed.routes)
+    assert len(component.loc[:, "city"].unique()) == 1
+    assert len(component.loc[:, "agency"].unique()) == 1
+    assert component.loc[:, "city"].unique()[0] == "Chicago"
+    assert component.loc[:, "agency"].unique()[0] == "Metra"
+    assert component.loc[:, "uniq_route_id"].str.startswith("Chicago_Metra_").all()
+
+
+@pytest.mark.skip(reason="refactor currently breaking test")
+def test_combine_different_feeds(cta_feed, metra_feed):
+    # TODO: consider making this a fixture
+    chicago_combined_dfs = combine_different_feeds([CTA_URL, METRA_URL])
+    assert "routes" in chicago_combined_dfs
+    assert "shapes" in chicago_combined_dfs
+    assert "trips" in chicago_combined_dfs
+    assert "stops" in chicago_combined_dfs
+    assert "stop_times" in chicago_combined_dfs
+    assert "transfers" in chicago_combined_dfs
+    # TODO: more detailed testing to make sure "Chicago_CTA_" and "Chicago_Metra_"
+    # are represented in the uniq_id column for each sub-sheet (for now, that
+    # seems minimally tested by test above this one)
+
+
+###################
+## NY Extract tests
+###################
+
+from app.scripts.extract_nyc_data import DATASETS, NY_TZ, extract_daily_data
+import datetime
+from parameterized import parameterized
+
+
+class ExtractNYData(TestCase):
+    @parameterized.expand(
+        [
+            [
+                "BUS_RIDERSHIP",
+                datetime.datetime(2023, 9, 7, tzinfo=NY_TZ),
+                320,
+                10_678,
+            ],
+            [
+                "SUBWAY_RIDERSHIP",
+                datetime.datetime(2023, 9, 7, tzinfo=NY_TZ),
+                428,
+                11_174,
+            ],
+            [
+                "BUS_RIDERSHIP",
+                datetime.datetime(2023, 2, 28, tzinfo=NY_TZ),
+                320,
+                10_868,
+            ],
+            [
+                "SUBWAY_RIDERSHIP",
+                datetime.datetime(2023, 2, 28, tzinfo=NY_TZ),
+                428,
+                10_532,
+            ],
+            [
+                "BUS_RIDERSHIP",
+                datetime.datetime(2022, 11, 5, tzinfo=NY_TZ),
+                255,
+                7_852,
+            ],
+            [
+                "SUBWAY_RIDERSHIP",
+                datetime.datetime(2022, 11, 5, tzinfo=NY_TZ),
+                428,
+                7_085,
+            ],
+        ]
+    )
+    def test_extract_daily_data(self, dataset, date, n_obs, first_station_ridership):
+        results = extract_daily_data(dataset=dataset, date=date)
+        self.assertIs(len(results), n_obs)
+        self.assertIs(
+            int(float(results[0]["total_ridership"])), first_station_ridership
+        )
+
+
+import datetime
+from app.scripts.extract_chi_ridership_data import extract_daily_data, DATASETS, CHI_TZ
+from unittest import TestCase
+
+##########################
+# CHI Extract tests
+##########################
+
+from parameterized import parameterized
+
+
+class ExtractChiData(TestCase):
+    @parameterized.expand(
+        [
+            [
+                DATASETS["BUS_RIDERSHIP"]["URL"],
+                datetime.datetime(2023, 2, 28, tzinfo=CHI_TZ),
+                125,
+            ],
+            [
+                DATASETS["BUS_RIDERSHIP"]["URL"],
+                datetime.datetime(2023, 3, 4, tzinfo=CHI_TZ),
+                94,
+            ],
+            [
+                DATASETS["SUBWAY_RIDERSHIP"]["URL"],
+                datetime.datetime(2023, 2, 28, tzinfo=CHI_TZ),
+                143,
+            ],
+            [
+                DATASETS["SUBWAY_RIDERSHIP"]["URL"],
+                datetime.datetime(2023, 3, 4, tzinfo=CHI_TZ),
+                143,
+            ],
+        ]
+    )
+    def test_extract_daily_data(self, url, date, expected):
+        resp = extract_daily_data(url, date)
+        self.assertIs(len(resp), expected)
---
+++ app/scripts/extract_chi_ridership_data.py
@@ -0,0 +1,238 @@
+import os
+from typing import List, Dict, Tuple
+from requests.models import Response
+from collections.abc import Callable
+import requests
+import time
+import pandas as pd
+import datetime
+import os
+import pytz
+from dotenv import load_dotenv
+from route_rangers_api.models import (
+    TransitRoute,
+    RidershipRoute,
+    TransitStation,
+    RidershipStation,
+)
+
+###########################
+# Load and define variables
+###########################
+load_dotenv()
+
+DATA_PORTAL_APP_TOKEN = os.getenv("DATA_PORTAL_APP_TOKEN")
+
+REQUEST_DELAY = 1
+RESULTS_PER_PAGE = 50000  # Max number of results for API
+TIMEOUT = 30
+
+CHI_TZ = pytz.timezone("America/Chicago")
+START_DATE = datetime.datetime(2023, 1, 1, tzinfo=CHI_TZ)
+END_DATE = datetime.datetime(2024, 1, 1, tzinfo=CHI_TZ)
+
+DATASETS = {
+    "BUS_RIDERSHIP": {
+        "URL": "https://data.cityofchicago.org/resource/jyb9-n7fm.json",
+        "ORDER_BY": "route",
+    },
+    "SUBWAY_RIDERSHIP": {
+        "URL": "https://data.cityofchicago.org/resource/5neh-572f.json",
+        "ORDER_BY": "stationname",
+    },
+}
+
+###########################
+# Data extraction
+###########################
+
+
+def make_request(url: str, params: Dict, session: Callable = None) -> Response:
+    """
+    Make a request to `url` and return the raw response.
+
+    This function ensure that the domain matches what is
+    expected and that the rate limit is obeyed.
+    """
+    time.sleep(REQUEST_DELAY)
+    print(f"Fetching {url}")
+    if session:
+        resp = session.get(url, params=params)
+    else:
+        resp = requests.get(url, params, timeout=TIMEOUT)
+    return resp
+
+
+def extract_daily_data(
+    url: str,
+    date: datetime.datetime,
+    app_token=DATA_PORTAL_APP_TOKEN,
+    session: Callable = None,
+    order_by: str = None,
+) -> List:
+    """
+    Extract daily data from a Chicago Subway or Bus ridership endpoint
+    storing it in a list with partial responses subject to the limit parameter.
+    One item in the list has up to "limit" items
+    """
+    # Create datetime objects for date query filter
+    start_date, end_date = build_start_end_date_str(date)
+
+    # Define parameters to pass into the request:
+    where_clause = f"date >= '{start_date}' AND date < '{end_date}'"
+
+    params = build_parameters(app_token, where_clause=where_clause, order_by=order_by)
+
+    resp = make_request(url, params, session)
+
+    # Status code handling
+    if resp.status_code == 200:
+        pass
+    elif (
+        resp.status_code == 202
+    ):  # API docs indicate that OK Status Codes are 200 and 202
+        print("202 response code")  ##TODO: Add retry functionality
+    else:
+        print("Not succesful requests")
+
+    resp_list = resp.json()
+
+    return resp_list
+
+
+def build_parameters(
+    app_token: str,
+    limit: int = None,
+    offset: int = None,
+    where_clause: str = None,
+    order_by: str = None,
+) -> Dict:
+    """
+    Build parameters to be passsed into the request call
+    """
+    params = {
+        "$$APP_TOKEN": app_token,
+        "$limit": limit,
+        "$offset": offset,
+        "$order": order_by,
+        "$where": where_clause,
+    }
+    return params
+
+
+def build_start_end_date_str(date: datetime.datetime) -> Tuple[str, str]:
+    """
+    Creates two strings to pass as filters on the query for
+    a request to the Chicago data Portal
+    """
+    start_date = date.astimezone(CHI_TZ)
+    time_delta = datetime.timedelta(days=1)
+    end_date = start_date + time_delta
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+
+    return start_date, end_date
+
+
+#####################
+# Data Ingestion
+#####################
+
+
+def ingest_bus_ridership(
+    start_date: datetime.date = START_DATE, end_date: datetime.date = END_DATE
+) -> None:
+    """
+    Ingest the Chicago bus ridership data to the RouteRidership table
+    starting from start_date and ending at end_date (inclusive)
+    """
+    session = requests.Session()
+    url = DATASETS["BUS_RIDERSHIP"]["URL"]
+    date = start_date
+    time_delta = datetime.timedelta(days=1)
+    while date <= end_date:
+        print(f"Extracting bus ridership for {date}")
+        date_ridership = extract_daily_data(url=url, date=date, order_by="route")
+        print(f"Ingesting bus ridership for {date}")
+        ingest_daily_bus_ridership(date_ridership, date)
+        date += time_delta
+
+
+def ingest_daily_bus_ridership(daily_bus_json, date: datetime.date) -> None:
+    """
+    Ingest Chicago bus data into the RouteRidership table. It ingests the data for one
+    day of ridership
+    """
+    for row in daily_bus_json:
+        print(row["route"])
+        try:
+            print(
+                f"Ingesting ridership data for route {str(row['route'])} - {row['date']}"
+            )
+            obs_route = TransitRoute.objects.filter(
+                city="CHI", route_id=row["route"]
+            ).first()
+            obs_route_id = obs_route.id
+            obs = RidershipRoute(
+                route_id=obs_route_id, date=date, ridership=row["rides"]
+            )
+            obs.save()
+        except:
+            print(f"Ingestion for row {row['route']} - {row['date']} not succesful")
+
+
+def ingest_subway_ridership(start_date: datetime.date, end_date: datetime.date) -> None:
+    """
+    Ingest the Chicago subway ridership data to the RouteRidership table
+    starting from start_date and ending at end_date (inclusive)
+    """
+    session = requests.Session()
+    url = DATASETS["SUBWAY_RIDERSHIP"]["URL"]
+    date = start_date
+    time_delta = datetime.timedelta(days=1)
+    while date <= end_date:
+        print(f"Extracting subway ridership for {date}")
+        date_ridership = extract_daily_data(url=url, date=date, order_by="station_id")
+        print(f"Starting subway ridership for {date}")
+        ingest_daily_subway_ridership(date_ridership, date)
+        date += time_delta
+
+
+def ingest_daily_subway_ridership(daily_subway_json, date: datetime.date) -> None:
+    """
+    Ingest Chicago subway data into the StationsRidership table. It ingests the data for one
+    day of ridership
+    """
+    for row in daily_subway_json:
+
+        try:
+            print(
+                f"Ingesting ridership data for station {row['station_id']} - {row['date']}"
+            )
+            obs_station = TransitStation.objects.filter(
+                city="CHI", station_id=row["station_id"]
+            ).first()
+            obs_station_id = obs_station.id
+            obs = RidershipStation(
+                station_id=obs_station_id, date=date, ridership=row["rides"]
+            )
+            obs.save()
+        except:
+            print(
+                f"Ingestion for row {row['station_id']} - {row['date']} not succesful"
+            )
+
+
+def run():
+    start = datetime.datetime(2023, 1, 1, tzinfo=CHI_TZ)
+    end = datetime.datetime(2023, 2, 1, tzinfo=CHI_TZ)
+    ingest_bus_ridership(start_date=start, end_date=end)
+    # ingest_subway_ridership(start_date=start, end_date=end)
+
+
+# if __name__ == "__main__":
+#     date = datetime.datetime(2023,7,9)
+#     resp = extract_daily_data(DATASETS["BUS_RIDERSHIP"]["URL"],date)
+#     for r in resp[:10]:
+#         print(r)
+#         print(r["route"])
