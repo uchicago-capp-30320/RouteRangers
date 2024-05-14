02035558da8e7b25421a226b6b5aa4883ffb69f7
Megan Moore <mm7148@uchicago.edu>
2024-05-14 16:07:07 -0500

    testing wo static deps

--- app/route_rangers_api/urls.py
+++ app/route_rangers_api/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 app_name = "app"
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("dashboard/<str:city>", views.dashboard, name="dashboard"),
     path("survey/<str:city>", views.survey, name="survey"),
