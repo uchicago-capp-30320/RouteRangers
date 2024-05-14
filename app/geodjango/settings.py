0400b6ab4c2ba1ab9feb29d9e6dd03f7d6b52401
Megan Moore <mm7148@uchicago.edu>
2024-05-14 14:27:17 -0500

    updating static paths

--- app/geodjango/settings.py
+++ app/geodjango/settings.py
@@ -155,8 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# print(f"Base dir is: {BASE_DIR}")
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+print(f"Base dir is: {BASE_DIR}")
+STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles")
 
 
 # Default primary key field type
