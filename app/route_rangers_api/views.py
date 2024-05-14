7922cb25e04ed8b674dd4181b9fd253c247bad33
Matthew Jackson <111554739+mbjackson-capp@users.noreply.github.com>
2024-05-14 16:16:39 -0500

    Add color-coded routes and extremely basic route and station tooltips to map (#103)
    * Get routes displaying on map
    
    Some notes:
    -Routes have a tooltip that displays on-click, showing the route's
    long name.
    -Colors are as-is (CTA bus routes are gray, Portland bus routes are
    dark bluish/purplish, New York's bus and subway routes are a wild
    mixture of colors, which we might want to set to a more neutral
    tone).
    -Metra routes are not showing up at all (and thus have no color);
    investigate whether this is a data ingestion issue, a validation
    issue (such as the color column needing .strip() to be 6 characters
    long for hex code), or something else such as a non-standard
    column name.
    
    * Add very basic on-hover tooltips to stops (not clusters)
    
    * delete print statements; reduce route opacity a bit
    
    ---------
    
    Co-authored-by: Matthew Jackson <mbjackson@Matthews-MacBook-Air.local>

--- app/route_rangers_api/views.py
+++ app/route_rangers_api/views.py
@@ -2,7 +2,7 @@ from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -10,6 +10,8 @@ from django.core.serializers import serialize
 
 from app.route_rangers_api.utils.city_mapping import CITY_CONTEXT
 from route_rangers_api.models import TransitRoute, TransitStation
+
+import json
 
 
 def home(request):
@@ -31,15 +33,28 @@ def dashboard(request, city: str):
     # get commute
 
     # get paths
-    routes = TransitRoute.objects.filter(city="CHI").values(
-        "geo_representation", "route_name", "color"
+    routes = TransitRoute.objects.filter(city=CITY_CONTEXT[city]["DB_Name"])  # .values(
+    # MultiLineString needs to be serialized into a GeoJson object for Leaflet to
+    # work with it.
+    # to serialize into GeoJson, need to get out entire Django model object, not just
+    # the .values("geo_representation", "route_name", "color")
+    # with .values() you get "AttributeError: 'dict' has no component 'meta'"
+    routes_json = serialize(
+        "geojson",
+        routes,
+        geometry_field="geo_representation",
+        fields=("route_name", "color"),
     )
 
     # stations
     stations = TransitStation.objects.values().filter(
         city=CITY_CONTEXT[city]["DB_Name"]
     )
-    lst_coords = [[point["location"].x, point["location"].y] for point in stations]
+
+    lst_coords = [
+        [point["location"].x, point["location"].y, point["station_name"]]
+        for point in stations
+    ]
 
     context = {
         "City": CITY_CONTEXT[city]["CityName"],
@@ -53,6 +68,7 @@ def dashboard(request, city: str):
         "feedback_class": "cs-li-link",
         "coordinates": CITY_CONTEXT[city]["Coordinates"],
         "stations": lst_coords,
+        "routes": routes_json,
     }
     return render(request, "dashboard.html", context)
 
