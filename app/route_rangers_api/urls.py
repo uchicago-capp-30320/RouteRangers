e68ce22544c973ed179f68f0ac27285cb43383e4
lisettesolis-capp <111702116+lisettesolis-capp@users.noreply.github.com>
2024-05-15 00:07:30 -0500

    seperate pages

--- app/route_rangers_api/urls.py
+++ app/route_rangers_api/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("dashboard/<str:city>", views.dashboard, name="dashboard"),
-    path("survey/<str:city>", views.survey, name="survey"),
+    path("survey/<str:city>/", views.survey_p1, name="survey"),
+    path("survey/<str:city>/2", views.survey_p2, name="survey_p2"),
+    path("survey/<str:city>/3", views.survey_p3, name="survey_p3"),
+    path("survey/<str:city>/4", views.survey_p4, name="survey_p4"),
     path("responses/<str:city>", views.responses, name="responses"),
 ]
