739e7217e36afca800d5a7a7144175d7fcfc3c6c
Benjamín Leiva Silva <96876463+benjaleivas@users.noreply.github.com>
2024-05-17 11:47:23 -0500

    Reverse route option enabled, route details hidden

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Initialize the map centered around Chicago (or city of interest)
-const map = L.map('map').setView([41.8781, -87.6298], 13);
+map = L.map('map').setView([41.8781, -87.6298], 13);
 
 // Layer design
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,8 +11,8 @@ L.Control.geocoder({
     defaultMarkGeocode: false
 })
     .on('markgeocode', function(e) {
-        const bbox = e.geocode.bbox;
-        const poly = L.polygon([
+        bbox = e.geocode.bbox;
+        poly = L.polygon([
             bbox.getSouthEast(),
             bbox.getNorthEast(),
             bbox.getNorthWest(),
@@ -21,50 +21,22 @@ L.Control.geocoder({
         map.fitBounds(poly.getBounds());
     })
     .on('routesfound', function(e) {
-        var routes = e.routes;
+        routes = e.routes;
         alert('Found ' + routes.length + ' route(s).');
     })
     .addTo(map);
 
 // Routing control
 L.Routing.control({
-    // waypoints: [
-    //     L.latLng(41.8853, -87.6246),
-    //     L.latLng(41.7906, -87.6001)
-    // ],
+    plan: new L.Routing.Plan([], {
+        createMarker: function(i, wp) {
+            return L.marker(wp.latLng, {
+                draggable: true
+            });
+        },
+        geocoder: L.Control.Geocoder.nominatim(),
+        reverseWaypoints: true
+    }),
     routeWhileDragging: true,
-    geocoder: L.Control.Geocoder.nominatim()
-    
-    })
-    .addTo(map);
+}).addTo(map);
 
-// // Function to create a button
-// function createButton(label, container) {
-//     const btn = L.DomUtil.create('button', '', container);
-//     btn.setAttribute('type', 'button');
-//     btn.innerHTML = label;
-//     return btn;
-// }
-
-// // Add a click event listener to the map
-// map.on('click', function(e) {
-//     var container = L.DomUtil.create('div'),
-//         startBtn = createButton('Start from this location', container),
-//         destBtn = createButton('Go to this location', container);
-
-//     L.popup()
-//         .setContent(container)
-//         .setLatLng(e.latlng)
-//         .openOn(map);
-
-//     // Add event listeners to the buttons
-//     L.DomEvent.on(startBtn, 'click', function() {
-//         control.spliceWaypoints(0, 1, e.latlng);
-//         map.closePopup();
-//     });
-
-//     L.DomEvent.on(destBtn, 'click', function() {
-//         control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-//         map.closePopup();
-//     });
-// });
